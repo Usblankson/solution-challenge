d92ad0fb452a623cd14dfba944c4adc9fd447408
uyohoiniblankson <uyohoiniblankson11@gmail.com>
2022-03-25 14:44:02 +0100

    Worked on app's Splash Screen (first screen)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 
 import 'package:flutter/material.dart';
+import 'package:solution_challenge/screens/auth/login_screen.dart';
+import 'package:solution_challenge/screens/splash_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
-    routes: {},
+    routes: {
+      '/': (context) => SplashScreen(),
+    },
   ));
 }
